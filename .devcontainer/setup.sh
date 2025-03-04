15d6cd7392bb7291cd40cb593d6a6490c0845bd1
Chetan Budathoki <chetanbudathoki@outlook.com>
2025-03-04 16:10:56 +0000

    basic setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,6 @@
-apk add php nginx
-apk add php-pgsql php-pdo_pgsql
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 
+apk update && apk upgrade -y
+
+apk add go=1.24.0-r0
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/youthcongress/nepal
+
+go 1.24.0
